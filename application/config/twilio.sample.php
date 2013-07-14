102fefd7a9b22640bd4723c611092b9077d8d3b7
Andrew Coulton <andrewnfcoulton@gmail.com>
2013-07-14 09:31:11 +0100

    Twilio config (use the secret version of the file for real creds)

---
+++ application/config/twilio.sample.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Config for the Twilio API. Put real stuff in here for a working app
+ * 
+ * @author    Andrew Coulton <andrew@ingenerator.com>
+ * @copyright 2013 inGenerator Ltd
+ */
+return array(
+	'sid'   => 'demo',
+	'token' => 'demo',
+	'incoming_number' => 'demo'
+);
