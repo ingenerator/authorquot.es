8eb2a828503d85b0fc5a00c31df2140b12e0512f
Andrew Coulton <andrewnfcoulton@gmail.com>
2013-07-14 02:28:44 +0100

    Set an insecure cookie salt to prevent errors
    jwplayer sets cookies which triggers the
    kohana insecure cookie warning.

--- application/bootstrap.php
+++ application/bootstrap.php
@@ -119,6 +119,8 @@ Kohana::modules(array(
   // 'userguide'  => MODPATH.'userguide',  // User guide and API documentation
 ));
 
+Cookie::$salt = 'insecure';
+
 /**
  * Set the routes. Each route must have a minimum of a name, a URI and a set of
  * defaults for the URI.
